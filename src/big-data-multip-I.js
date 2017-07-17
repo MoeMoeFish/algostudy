c33595a0a37c7c70a1c7afb077f5bda25f5af0ba
FishYu <fish@tanbagua.com>
2017-07-17 18:00:41 +0800

    daily

---
+++ src/big-data-multip-I.js
@@ -0,0 +1,22 @@
+// 分治法解决大叔相乘
+// http://cnn237111.blog.51cto.com/2359144/1201901
+
+const multip = function(num1, num2) {
+  if (num1.length + num2.length < 16) {
+    return parseInt(num1) * parseInt(num2)
+  }
+
+  let n = Math.max(num1.length, num2.length)
+  n = Math.ceil(n / 2)
+
+  
+
+}
+
+// num1, num2 is array
+const multipInner = function(num1, num2) {
+  
+  
+}
+
+module.exports = multip
